5b8faad9467a41457174afc29ba658f674872d04
Evgenii Levchenko <evgeniy.levchnko@gmail.com>
2021-10-24 21:59:46 +0300

    git remote add origin https://github.com/EvgeniyLevchenko/counter.git git branch -M main git push -u origin main

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var tapOrHold: UIButton!
+    
+    let formatter = NumberFormatter()
+    var counter: Int = 0
+    var timer: Timer = Timer()
+    
+    @IBAction func resetButton(_ sender: Any) {
+        self.counter = 0
+        self.counterLabel.text = String(self.counter)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(longPressed))
+        longPressRecognizer.numberOfTouchesRequired = 1
+        longPressRecognizer.minimumPressDuration = 0.5
+        tapOrHold.addGestureRecognizer(longPressRecognizer)
+    }
+    
+    @IBAction func tapped(_ sender: UIButton) {
+        counter += 1
+        counterLabel.text = String(counter)
     }
 
-
+    @objc func longPressed(_ sender: UILongPressGestureRecognizer) {
+        switch sender.state {
+        case .began:
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target:self, selector: #selector(tapped(_:)), userInfo: nil, repeats: true)
+        case .changed:
+            counter = formatter.number(from: counterLabel.text!)!.intValue + 1
+            counterLabel.text = String(counter)
+        case .ended:
+            timer.invalidate()
+        default:
+            break
+        }
+    }
 }
+
 
